5ce2cb8b289ba0d6fc4a3d94a38e9df3b7d36637
muhammad-nuaman <muhammadnuaman92@gmail.com>
2024-04-05 18:35:20 +0500

    done

---
+++ day-3/Q9/app.js
@@ -0,0 +1,16 @@
+"use strict";
+/*
+# Question 9:
+
+# Favorite Number:
+
+> Store your favorite number in a variable.
+
+> Then, create a message revealing your favorite number. Print that message.
+
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+// Store Favourite Number 
+const myFavNumber = 7;
+// Print Favourite Number
+console.log(`My favorite number is ${myFavNumber}`);
---
+++ day-3/Q9/app.ts
@@ -0,0 +1,16 @@
+/*
+# Question 9: 
+
+# Favorite Number: 
+
+> Store your favorite number in a variable. 
+
+> Then, create a message revealing your favorite number. Print that message.
+
+*/
+
+// Store Favourite Number 
+const myFavNumber = 7;
+
+// Print Favourite Number
+console.log(`My favorite number is ${myFavNumber}`);
